f3525de1e5fe23cbe0968895f5a2598593c0537a
Satyajitmittra <34593389+Satyajitmittra@users.noreply.github.com>
2021-01-25 02:18:12 +0530

    Initial commit

---
+++ custom/hr_recruitment/__manifest__.py
@@ -0,0 +1,41 @@
+
+
+{
+    'name': 'Recruitment',
+    'version': '1.0',
+    'category': 'Productivity',
+    'sequence': -100,
+    'summary': 'Applicant Tracking System',
+    'description': "",
+    'website': '',
+    'depends': [
+        'hr',
+        'calendar',
+        'fetchmail',
+        'utm',
+        'attachment_indexation',
+        'web_tour',
+        'digest',
+    ],
+    'data': [
+        'security/hr_recruitment_security.xml',
+        'security/ir.model.access.csv',
+        'data/hr_recruitment_data.xml',
+        'data/digest_data.xml',
+        'data/hr_recruitment_templates.xml',
+        'views/hr_recruitment_views.xml',
+        'views/res_config_settings_views.xml',
+        'views/hr_recruitment_templates.xml',
+        'views/hr_department_views.xml',
+        'views/hr_job_views.xml',
+        'views/mail_activity_views.xml',
+        'views/digest_views.xml',
+        'wizard/applicant_refuse_reason_views.xml',
+    ],
+    'demo': [
+        'data/hr_recruitment_demo.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+}
